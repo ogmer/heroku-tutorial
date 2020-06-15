e33d5ceae562c775876a8065ce92dd8e60d20169
Ogm <52206772+ogmeat@users.noreply.github.com>
2020-06-16 06:55:05 +0900

    Update HighandLow.送信.php

--- PHP/HighandLow.送信.php
+++ PHP/HighandLow.送信.php
@@ -1,7 +1,7 @@
 <?php
-	$cards=array("Jk.png","01.png","02.png","03.png","04.png","05.png","06.png",
+	$cards = array("Jk.png","01.png","02.png","03.png","04.png","05.png","06.png",
 	"07.png","08.png","09.png","10.png","11.png","12.png","13.png");
-	$num1=mt_rand(0,13);
+	$num1 = mt_rand(0,13);
 ?>
 <html>
 	<head>
