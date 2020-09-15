b0f0b46fb72a38c61a315e8d39a0a7a26bd0d1dd
Ogm <52206772+ogmeat@users.noreply.github.com>
2020-09-16 08:48:27 +0900

    Update HighandLow.send.php

--- PHP/HighandLow.send.php
+++ PHP/HighandLow.send.php
@@ -1,5 +1,5 @@
 <?php
-	$cards = array("Jk.png","01.png","02.png","03.png","04.png","05.png","06.png",
+	$cards = array("jk.png","01.png","02.png","03.png","04.png","05.png","06.png",
 	"07.png","08.png","09.png","10.png","11.png","12.png","13.png");
 	$num1 = mt_rand(0,13);
 ?>
