510baadb66d7665a7ddc1d6df86d94f9ce9aea1d
Ogm <watershed@keemail.me>
2021-03-31 07:38:00 +0900

    Update HighandLow.send.php

--- src/HighandLow.send.php
+++ src/HighandLow.send.php
@@ -2,7 +2,6 @@
 	$cards = array("jk.png","01.png","02.png","03.png","04.png","05.png","06.png",
 	"07.png","08.png","09.png","10.png","11.png","12.png","13.png");
 	$num1 = mt_rand(0,13);
-?>
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -11,7 +10,7 @@
     <div align="center">
     <font size="6">High&amp;Lowゲーム</font>
 	<hr>
-	<form action="highAndLowSend.php"method="post">
+	<form action="HighAndLow.recieve.php"method="post">
 	<?php
 	echo '<img src="../cards/',$cards[$num1],'">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
 	echo '<img src="../cards/bg.png">';
