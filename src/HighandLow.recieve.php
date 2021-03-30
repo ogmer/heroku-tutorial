c2af377ce1cefce87aa3123a6d6990d9a1c9f9da
Ogm <watershed@keemail.me>
2021-03-31 07:45:44 +0900

    Update HighandLow.recieve.php

--- src/HighandLow.recieve.php
+++ src/HighandLow.recieve.php
@@ -34,7 +34,7 @@ $LightCard = $num2;
 		}  elseif ($LowNum <= $result){
 		   echo "You Lose...".PHP_EOL.PHP_EOL;
 		}  
-		echo'<a href="highAndLows03.php">もう一度挑戦</a>';
+		echo'<a href="HighAndLows.send.php">もう一度挑戦</a>';
 		?>
 		</div>
     </body>
