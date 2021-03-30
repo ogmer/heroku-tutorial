40e4fc55dad807899e528447cdf624ec969e7f4b
Ogm <watershed@keemail.me>
2021-03-31 07:33:04 +0900

    Update HighandLow.recieve.php

--- src/HighandLow.recieve.php
+++ src/HighandLow.recieve.php
@@ -2,37 +2,40 @@
 $cards = array("jk.png","01.png","02.png","03.png","04.png","05.png","06.png",
 		"07.png","08.png","09.png","10.png","11.png","12.png","13.png");
 
-$hNum = $_POST['leftCard'];
-$lNum = $_POST['select'];
+$HighNum = $_POST['leftCard'];
+$LowNum = $_POST['select'];
 $num2 = mt_rand(0,13);
 
-$lightCard = $num2;
+$LightCard = $num2;
 ?>
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	</head>
     <body>
-    <div align="center">
-    <font size="6">High&amp;Lowゲーム</font>
-	<hr>
-	<?php
-	echo '<img src="../cards/',$cards[$hNum],'">&nbsp;&nbsp;&nbsp;&nbsp;';
-	echo '<img src="../cards/',$cards[$lightCard],'"><br>';
-	echo "",$lNum,"を選択しました。<br><br>";
-	   if ($hNum < $lightCard){
-	   $result = "High";
-	}  elseif ($hNum>$lightCard){
-	   $result = "Low";
-	}  else  {
-	   $result = $lNum;
-	}
-	   if ($lNum == $result){
-	   echo "You Win!<br><br>";
-	}  elseif ($lNum <> $result){
-           echo "You Lose...<br><br>";
-	}  echo'<a href="highAndLows03.php">もう一度挑戦</a>';
-	?>
-	</div>
+	    <div align="center">
+	    <font size="6">High&amp;Lowゲーム</font>
+		<hr>
+		<?php
+		echo '<img src="../cards/',$cards[$HighNum],'">&nbsp;&nbsp;&nbsp;&nbsp;';
+		echo '<img src="../cards/',$cards[$LightCard],'">'.PHP_EOL.PHP_EOL;
+		echo "",$LowNum,"を選択しました。".PHP_EOL.PHP_EOL;
+
+		if ($HighNum < $LightCard){
+		   $result = "High";
+		}  elseif ($HighNum > $LightCard){
+		   $result = "Low";
+		}  else  {
+		   $result = $LowNum;
+		}
+
+		if ($LowNum == $result){
+		   echo "You Win!".PHP_EOL.PHP_EOL;
+		}  elseif ($LowNum <= $result){
+		   echo "You Lose...".PHP_EOL.PHP_EOL;
+		}  
+		echo'<a href="highAndLows03.php">もう一度挑戦</a>';
+		?>
+		</div>
     </body>
 </html>
