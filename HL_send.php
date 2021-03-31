4465a1ffce56f88d850b71de6af8e97390662ff9
Ogm <watershed@keemail.me>
2021-03-31 18:49:44 +0900

    Update and rename index.php to HL_send.php

--- HL_send.php
+++ HL_send.php
@@ -11,7 +11,7 @@
     <div align="center">
     <font size="6">High&amp;Lowゲーム</font>
 		<hr>
-		<form action="HighAndLow.recieve.php"method="post">
+		<form action="HighLow.recieve.php"method="post">
 		<?php
 		//echo '<img src="../cards/',$cards[$num1],'">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
 		echo sprinf( '-%8s',<img src="../cards/',$cards[$num1],'">');
