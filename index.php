a8e740ebf9a9a74cef5dbf0f0a4c45eb2fb5b9b3
Ogm <watershed@keemail.me>
2021-03-31 19:22:42 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
 echo "Hello ogmer!! Hello everyone!!!";
+<html>
+  <head>
+  <title>word</title>
+  </head>
+  <body>
+  </body>
+  <style>
+  body {
+  text-align: center;
+  backgroung-color: skyblue;
+  </style>
+</html>
